d0bfae271f4117ff0df81a1c3440eb892211e8eb
SamSamhuns <samhunsadamant@gmail.com>
2021-08-08 18:18:29 +0400

    Add tf1/tf2 label in conv files

---
+++ python/conversion/conv_tf2_hdf5_to_pb.py
@@ -0,0 +1,30 @@
+import os
+import argparse
+import tensorflow as tf
+
+
+def save_pb(model_path, save_pb_dir):
+    model = tf.keras.models.load_model(model_path)
+    input_name = model.input
+    output_name = model.output
+    print(f"input_name: {input_name}")
+    print(f"output_name: {output_name}")
+
+    os.makedirs(save_pb_dir, exist_ok=True)
+    model.save(save_pb_dir)
+
+
+def main(model_path, save_pb_dir):
+    save_pb(model_path, save_pb_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Dump hdf5 model to pb file')
+    parser.add_argument('-m', '--model_path', type=str,
+                        help='path to hdf5/h5 model', default="")
+    parser.add_argument('-sp', '--save_pb_dir', type=str,
+                        help='folder path to save pb file', default="models/savedmodel")
+    args = parser.parse_args()
+
+    main(args.model_path,
+         args.save_pb_dir)
