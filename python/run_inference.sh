b8ea46806e9ccd3e041f6514e11628f3f3f21c4d
SamSamhuns <samhunsadamant@gmail.com>
2021-08-11 11:06:59 +0400

    Add xla flag set in run script

--- python/run_inference.sh
+++ python/run_inference.sh
@@ -2,6 +2,7 @@
 eval "$(conda shell.bash hook)"
 conda activate fastseg       # replace with the right conda env
 
+export TF_XLA_FLAGS="--tf_xla_enable_xla_devices --tf_xla_auto_jit=2 --tf_xla_cpu_global_jit"
 export PYTHONPATH=${PYTHONPATH}:./
 export PYTHONPATH=${PYTHONPATH}:./utils/
 
