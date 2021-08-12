3829a9b3944f6fa1808100c6e069a263c5737f1f
SamSamhuns <samhunsadamant@gmail.com>
2021-08-12 20:48:15 +0400

    Remove specific fnames

--- python/utils/plot.py
+++ python/utils/plot.py
@@ -25,15 +25,10 @@ def plot_fps_arr(data_dict_list, title,  savepath=None):
 
 
 def main():
-    tf_graphdef_data = [{'x': np.load("orig_pb.npy"), 'color': 'b', 'label': 'Original',
+    tf_graphdef_data = [{'x': np.load("arr1.npy"), 'color': 'b', 'label': 'Original',
                          'linestyle': 'dashed', 'alpha': 0.7},
-                        {'x': np.load("multit_pb.npy"), 'color': 'r', 'label': 'Improved'}]
-    plot_fps_arr(tf_graphdef_data, title="TFGraphDef",  savepath="tf_graphdef_fps.png")
-
-    tf_graphdef_data = [{'x': np.load("orig_mp.npy"), 'color': 'b', 'label': 'Original',
-                         'linestyle': 'dashed', 'alpha': 0.7},
-                        {'x': np.load("multit_mp.npy"), 'color': 'r', 'label': 'Improved'}]
-    plot_fps_arr(tf_graphdef_data, title="MediaPipe",  savepath="mediapipe_fps.png")
+                        {'x': np.load("arr2.npy"), 'color': 'r', 'label': 'Improved'}]
+    plot_fps_arr(tf_graphdef_data, title="Title",  savepath="figure.png")
 
 
 if __name__ == "__main__":
