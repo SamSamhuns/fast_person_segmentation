6fd717a51364094a151f5e5cd4ab2d67bdf09f92
SamSamhuns <samhunsadamant@gmail.com>
2021-08-29 18:12:22 +0400

    Refmt pb cpp inf cmakelist & modularize tflite cpp inf

--- cpp/cpp_tflite/include/common/common.h
+++ cpp/cpp_tflite/include/common/common.h
@@ -2,6 +2,8 @@
 #define COMMON_H
 
 #include <string>
+#include <stdio.h>
+#include "tensorflow/lite/interpreter.h"
 
 #define TFLITE_MINIMAL_CHECK(x)                                                \
   if (!(x)) {                                                                  \
@@ -11,6 +13,7 @@
 
 struct Settings {
   std::string model_path = "";
+  float threshold = 0.63;
   bool verbose = true;
   bool accel = false;
   bool allow_fp16 = false;
@@ -20,7 +23,17 @@ struct Settings {
   int number_of_threads = 4;
 };
 
+struct IOShape {
+  int index;
+  int bsize;
+  int height;
+  int width;
+  int channels;
+};
+
+Settings get_settings(std::string model_path);
 std::string get_basename(std::string full_path);
-Settings get_settings(std::string model_type);
+void print_model_struct(std::unique_ptr<tflite::Interpreter>& interpreter);
+std::tuple<IOShape, IOShape>get_input_output_dims(Settings &settings, std::unique_ptr<tflite::Interpreter> &interpreter);
 
 #endif
--- cpp/cpp_tflite/src/common.cpp
+++ cpp/cpp_tflite/src/common.cpp
@@ -4,7 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <sys/stat.h>
+#include <tuple>
 #include <unordered_map>
+// tf header files
+#include "tensorflow/lite/interpreter.h"
+#include "tensorflow/lite/kernels/register.h"
+#include "tensorflow/lite/model.h"
+#include "tensorflow/lite/optional_debug_tools.h"
+#include "tensorflow/lite/tools/gen_op_registration.h"
 
 std::string get_basename(std::string full_path) {
   // get model name from full model path
@@ -15,21 +23,85 @@ std::string get_basename(std::string full_path) {
   return str;
 }
 
-Settings get_settings(std::string model_type) {
-  Settings s;
-  std::string model_path;
-  const static std::unordered_map<std::string, int> model_num_map{{"m1", 1}};
+bool does_file_exist(const std::string &fpath) {
+  // if check if a file exists in fpath
+  struct stat buffer;
+  return (stat(fpath.c_str(), &buffer) == 0);
+}
 
-  switch (model_num_map.count(model_type) ? model_num_map.at(model_type) : 0) {
-  case 1:
-    std::cout << "Selfie Seg Model 256x144 chosen" << std::endl;
-    model_path = "../model_zoo/144x256_float32.tflite";
-    break;
-  case 0: // for the undefined case
-    std::cout << "Error. Model type " << model_type << " not available"
-              << std::endl;
+Settings get_settings(std::string model_path) {
+  Settings s;
+  if (does_file_exist(model_path)) {
+    std::cout << "tflite model found at: " << model_path << '\n';
+  } else {
+    std::cout << "Invalid tflite model path: " << model_path << '\n';
     exit(1);
   }
+
   s.model_path = model_path;
   return s;
+}
+
+void print_model_struct(std::unique_ptr<tflite::Interpreter>& interpreter) {
+  std::cout << "Printing model layer name and shapes:" << std::endl;
+  int t_size = interpreter->tensors_size();
+  for (int i = 0; i < t_size; i++) {
+    if (interpreter->tensor(i)->name)
+      std::cout << i << ": " << interpreter->tensor(i)->name << ", "
+                << interpreter->tensor(i)->bytes << ", "
+                << interpreter->tensor(i)->type << ", "
+                << interpreter->tensor(i)->params.scale << ", "
+                << interpreter->tensor(i)->params.zero_point << std::endl;
+    }
+}
+
+std::tuple<IOShape, IOShape>
+get_input_output_dims(Settings &settings,
+                      std::unique_ptr<tflite::Interpreter> &interpreter) {
+  // get input/output dimension from the input/output tensor metadata
+  int input_index = 0;
+  int output_index = 0;
+  int input = interpreter->inputs()[input_index];
+  TfLiteIntArray *in_dims = interpreter->tensor(input)->dims;
+  int in_bsize = in_dims->data[0];
+  int in_height = in_dims->data[1];
+  int in_width = in_dims->data[2];
+  int in_channels = in_dims->data[3];
+  int output = interpreter->outputs()[output_index];
+  TfLiteIntArray *out_dims = interpreter->tensor(output)->dims;
+  int out_bsize = out_dims->data[0];
+  int out_height = out_dims->data[1];
+  int out_width = out_dims->data[2];
+  int out_channels = out_dims->data[3];
+
+  const std::vector<int> inputs = interpreter->inputs();
+  const std::vector<int> outputs = interpreter->outputs();
+
+  if (settings.verbose) {
+    printf("##### Printing INPUT/OUTPUT NAME and SHAPES #####\n");
+    printf("Input shape: [%d,%d,%d,%d]\n", in_bsize, in_height, in_width,
+           in_channels);
+    std::cout << "input(0) name: " << interpreter->GetInputName(0) << "\n";
+    std::cout << "number of inputs: " << inputs.size() << "\n";
+    printf("Output shape: [%d,%d,%d,%d]\n", out_bsize, out_height, out_width,
+           out_channels);
+    std::cout << "output(0) name: " << interpreter->GetOutputName(0) << "\n";
+    std::cout << "number of outputs: " << outputs.size() << "\n";
+  }
+
+  IOShape input_shape;
+  IOShape output_shape;
+
+  input_shape.index = input_index;
+  input_shape.bsize = in_bsize;
+  input_shape.height = in_height;
+  input_shape.width = in_width;
+  input_shape.channels = in_channels;
+
+  output_shape.index = output_index;
+  output_shape.bsize = out_bsize;
+  output_shape.height = out_height;
+  output_shape.width = out_width;
+  output_shape.channels = out_channels;
+  return std::make_tuple(input_shape, output_shape);
 }
